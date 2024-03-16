c2f2a9a07c031371937795332c45dbc8b631012c
saturogojo12 <saransh.sharma1202@gmail.com>
2024-03-16 09:54:28 +0530

    setup

--- src/exception.py
+++ src/exception.py
@@ -6,7 +6,7 @@ def error_message_detail(error,error_detail:sys):
         file_name,exc_tb.tb_lineno,str(error))
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
         return self.error_message
